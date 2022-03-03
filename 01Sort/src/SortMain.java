a81728053651a2f2b285f2d4aefd139b5c3eeddc
Toygoon <toygoon@gmail.com>
2022-03-03 18:45:23 +0900

    Separate with linear sort and normal sort

--- 01Sort/src/SortMain.java
+++ 01Sort/src/SortMain.java
@@ -1,10 +1,22 @@
 import sort.*;
 
 public class SortMain {
-    public static void main(String[] args) {
+    public static void normalSort() {
         Integer[] a = {10, 4, 5, 2, 1, 8, 3, 6};
 
         Shell.sort(a);
         Shell.show(a);
+    }
+
+    public static void linearSort() {
+        int[] a = {10, 4, 5, 8, 1, 8, 3, 6}, b;
+        b = Counting.sort(a, 11);
+        for(int i=0; i<b.length; i++)
+            System.out.print(b[i] + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        linearSort();
     }
 }
