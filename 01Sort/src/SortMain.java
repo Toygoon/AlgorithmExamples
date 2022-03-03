008ba8a47c250e47fca4448d04ce78d6491f4472
Toygoon <toygoon@gmail.com>
2022-03-03 17:57:31 +0900

    Sort examples

--- 01Sort/src/SortMain.java
+++ 01Sort/src/SortMain.java
@@ -1,5 +1,10 @@
+import sort.Insertion;
+
 public class SortMain {
     public static void main(String[] args) {
+        Integer[] a = {10, 4, 5, 2, 1, 8, 3, 6};
 
+        Insertion.sort(a);
+        Insertion.show(a);
     }
 }
