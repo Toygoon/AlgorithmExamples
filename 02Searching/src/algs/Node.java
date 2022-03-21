ccd0a7eff5dac9d0c1abe91030bb63135cdea73a
Toygoon <toygoon@gmail.com>
2022-03-21 19:33:42 +0900

    Node class

---
+++ 02Searching/src/algs/Node.java
@@ -0,0 +1,21 @@
+package algs;
+
+/**
+ * Single Node
+ * A pair of key and value
+ *
+ * @param <K> : Key
+ * @param <V> : Value
+ */
+
+public class Node<K, V> {
+    K key;
+    V value;
+    Node<K, V> next;
+
+    public Node(K key, V value, Node<K, V> next) {
+        this.key = key;
+        this.value = value;
+        this.next = next;
+    }
+}
