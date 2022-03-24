c6d6ab93b183cc35caabf85052bbed8d7855d15a
Toygoon <toygoon@gmail.com>
2022-03-25 00:36:13 +0900

    Base

--- 02Searching/src/SearchingMain.java
+++ 02Searching/src/SearchingMain.java
@@ -1,5 +1,8 @@
+import util.FrequencyCounter;
+
 public class SearchingMain {
     public static void main(String[] args) {
-
+        FrequencyCounter fc = new FrequencyCounter();
+        fc.count();
     }
 }
---
+++ 02Searching/src/util/FrequencyCounter.java
@@ -0,0 +1,80 @@
+package util;
+
+import algorithm.SequentialSearchST;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FrequencyCounter {
+    private static final int DEFAULT_MIN_LENGTH = 8;
+    private static int minLength;
+
+    public FrequencyCounter() {
+        this(DEFAULT_MIN_LENGTH);
+    }
+
+    public FrequencyCounter(int minLength) {
+        this.minLength = minLength;
+    }
+
+    public FrequencyCounter(String[] args) {
+        this(Integer.parseInt(args[0]));
+    }
+
+    public static void count() {
+        SequentialSearchST<String, Integer> st = new SequentialSearchST<>();
+        File file = null;
+        JFileChooser jFileChooser = new JFileChooser();
+        String word, maxKey = "";
+        int maxValue = 0;
+        long start, end;
+
+        if (jFileChooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION)
+            file = jFileChooser.getSelectedFile();
+        else {
+            JOptionPane.showMessageDialog(null, "Select the file",
+                    "Error", JOptionPane.ERROR_MESSAGE);
+        }
+
+        Scanner sc = null;
+        // Calculate elapsed time
+        start = System.currentTimeMillis();
+
+        /* Save the key-value pairs to the LinkedList
+          Key : Word
+          Value : Frequency of a word
+         */
+        try {
+            sc = new Scanner(file);
+            start = System.currentTimeMillis();
+
+            while (sc.hasNext()) {
+                word = sc.next();
+                // Ignore length is less than minimum length
+                if (word.length() < minLength)
+                    continue;
+                // If the word not exists in the LinkedList, put the word to it
+                if (!st.contains(word))
+                    st.put(word, 1);
+                // If the word exist in the LinkedList, increase the count of it
+                else
+                    st.put(word, st.get(word) + 1);
+            }
+
+            for(String s : st.keys())
+                // Find out maximum value from the LinkedList like Selection Search
+                if (st.get(s) > maxValue) {
+                    maxValue = st.get(s);
+                    maxKey = s;
+                }
+
+            end = System.currentTimeMillis();
+            System.out.println(maxKey + " " + maxValue);
+            System.out.println("Elapsed time : " + (end - start) + "ms");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
